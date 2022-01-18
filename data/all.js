090e379063209855538450a9622a83c1c4bc7ab3
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2022-01-18 22:43:59 +0800

    Fix Tailwind and Change all.json to all.js Tailwind compilation was too large, now it was fixed from 5mb to a few kilobytes. Also all.json instead of all.js allows us to update the datapoints files without having to change all.json anymore.

---
+++ data/all.js
@@ -0,0 +1,8 @@
+let activityLevels = require('./activity-levels.json');
+let activityLocation = require('./activity-location.json');
+let activityPlanned = require('./activity-planned.json');
+let maskTypes = require('./mask-types.json');
+let results = require('./results.json');
+let ventilationRates = require('./ventilation-rates.json');
+
+module.exports = { activityLevels, activityLocation, activityPlanned, maskTypes, results, ventilationRates } 
