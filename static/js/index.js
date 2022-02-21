2959ce9fc01f9b812cd9154f943ae84501a15399
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2022-02-21 20:28:46 +0800

    Change Back To Top Scroll Don't use it by default, but only use it in specific pages.

--- static/js/index.js
+++ static/js/index.js
@@ -64,6 +64,7 @@ function goTo(path) {
         history.pushState({}, document.title, path);
         bindSeamlessLinks();
         language ? setLanguage(language) : setLanguage('english');
+        $(window).off('scroll');
     });
 }
 
@@ -211,6 +212,24 @@ window.onpopstate = function(event) {
 function setLanguage(language) {
     $('.' + language +'-text').each(function() {
         $(this).addClass('is-active');
+    });
+}
+
+function bindToTopScroll() {
+    var to_top_btn = $('#to_top_btn');
+
+    // detect scroll distance for toggling to_top_button
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 20) {
+            to_top_btn.addClass('show');
+        } else {
+            to_top_btn.removeClass('show');
+        };
+    });
+
+    to_top_btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
     });
 }
 
